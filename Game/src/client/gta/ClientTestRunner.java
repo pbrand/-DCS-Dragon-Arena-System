21339d13826cc23a2d28a918a72332124464699f
Raies1000 <raiesafzal@live.nl>
2015-03-30 14:39:06 +0200

    Added test runner disconnect

--- Game/src/client/gta/ClientTestRunner.java
+++ Game/src/client/gta/ClientTestRunner.java
@@ -1,24 +1,33 @@
 package client.gta;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.List;
+
+import common.IPlayerController;
 
 import client.ClientMain;
 
 public class ClientTestRunner {
 
 	private static List<GtaClient> clients;
+	private static String address;
 
 	public static void main(String[] args) {
 		String file = "SC2_Edge_Detailed";
 		clients = FileParser.getClientsData(file);
 		executeClients(clients, args[0]);
+		address = args[0];
 	}
 
 	private static void executeClients(List<GtaClient> clients,
 			String mainAddress) {
 		for (int i = 0; i < clients.size() && i < 101; i++) {
 			String[] arguments = { mainAddress, "p_" + clients.get(i).getId() };
-			clientRunner(arguments);
+			long runtime = 0;
+			clientRunner(arguments, runtime);
 			if (i < clients.size()) {
 				double time = (clients.get(i + 1).getTimestamp() - clients.get(i).getTimestamp()) / 1000;
 				System.out.println("Wait for: " + time);
@@ -31,11 +40,13 @@ public class ClientTestRunner {
 		}
 	}
 
-	private static void clientRunner(final String[] arguments) {
+	private static void clientRunner(final String[] arguments, long runtime) {
 		Runnable myRunnable = new Runnable() {
 			public void run() {
 				try {
 					ClientMain.main(arguments);
+					Thread.sleep(runtime);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,6 +54,17 @@ public class ClientTestRunner {
 		};
 		Thread thread = new Thread(myRunnable);
 		thread.start();
+	}
+	
+	private static void disconnectPlayer(String player) {
+		try {
+			IPlayerController RMIServer = (IPlayerController) Naming
+					.lookup("rmi://" + address + "/"
+							+ player);
+			
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- Game/src/common/Log.java
+++ Game/src/common/Log.java
@@ -1,9 +1,26 @@
 package common;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Log {
 	
 	public static void log(String tag, String text) {
-		System.out.println("[" + tag + "]: " + text);
+		String file = "log.txt";
+		String string = "[" + tag + "]: " + text;
+		System.out.println(string);
+		saveLog(file, string);
+	}
+
+	private static void saveLog(String file, String text) {
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(
+				new FileWriter(file, true)))) {
+			out.println(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
